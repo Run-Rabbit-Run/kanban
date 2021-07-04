902dba44c5aed3379a0aae3b49f61822a9fb6bc2
rabbit <barateligeorge@yandex.ru>
2021-07-04 13:13:47 +0300

    added close button for createTask popup and disabled submit button when inputs are empty

--- src/components/createTask/createTask.jsx
+++ src/components/createTask/createTask.jsx
@@ -1,6 +1,7 @@
 import { connect } from "react-redux";
-import { textNewTaskUpdate, titleNewTaskUpdate } from "../../redux/createTask/createTask.actions";
+import { textNewTaskUpdate, titleNewTaskUpdate,statusNewTaskChange } from "../../redux/createTask/createTask.actions";
 import { addNewTask } from "../../redux/tasks/tasks.actions";
+import { FaRegWindowClose } from "react-icons/fa";
 import _ from "lodash";
 import "./createTask.scss";
 
@@ -18,15 +19,20 @@ const actionCreators = {
   textNewTaskUpdate,
   titleNewTaskUpdate,
   addNewTask,
+  statusNewTaskChange,
 }
 
-const CreateTask = ({text, title, status, textNewTaskUpdate, titleNewTaskUpdate, addNewTask}) => {
+const CreateTask = ({text, title, status, textNewTaskUpdate, titleNewTaskUpdate, addNewTask, statusNewTaskChange}) => {
   const handleTextUpdate = (e) => {
     textNewTaskUpdate(e.target.value);
   };
   
   const handleTitleUpdate = (e) => {
     titleNewTaskUpdate(e.target.value);
+  };
+  
+  const handleClosePopup = () => {
+    statusNewTaskChange(status);
   };
   
   const handleAddTask = (e) => {
@@ -42,37 +48,48 @@ const CreateTask = ({text, title, status, textNewTaskUpdate, titleNewTaskUpdate,
 
   return (
     status === 'editing' ? (
-      <form
-        className="create-task"
-        onSubmit={handleAddTask}
-      >
-        <label
-          htmlFor="title"
-        >Title</label>
-        <input
-          className="create-task__title"
-          id="title"
-          type="text"
-          value={title}
-          onChange={handleTitleUpdate}
-        />
-
-        <label
-          htmlFor="text"
-        >Task desctiption</label>
-        <textarea
-          className="create-task__text"
-          id="text"
-          name="text"
-          value={text}
-          onChange={handleTextUpdate}
-        />
-
-        <button
-          className="create-task__button"
-          type="submit"
-        >Add Task</button>
-      </form>
+      <div className="create-task-wrapper">
+        <form
+          className="create-task"
+          onSubmit={handleAddTask}
+        >
+          <label
+            htmlFor="title"
+          >Title</label>
+          <input
+            className="create-task__title"
+            id="title"
+            type="text"
+            value={title}
+            onChange={handleTitleUpdate}
+          />
+  
+          <label
+            htmlFor="text"
+          >Task desctiption</label>
+          <textarea
+            className="create-task__text"
+            id="text"
+            name="text"
+            value={text}
+            onChange={handleTextUpdate}
+          />
+  
+          <button
+            className="create-task__submit-button"
+            type="submit"
+            disabled={text === '' || title === ''}
+          >Add Task</button>
+  
+          <button
+            className="create-task__close-button"
+            type='button'
+            onClick={handleClosePopup}
+          >
+            <FaRegWindowClose size='1.5em' />
+          </button>
+        </form>
+      </div>
     ) : null
   );
 };
